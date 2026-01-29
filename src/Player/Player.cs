788ab89eb15385c59893b5448afd22899ee00d74
IOSA33 <sedinkin33@gmail.com>
2026-01-29 22:23:08 +0200

    first commit

---
+++ src/Player/Player.cs
@@ -0,0 +1,23 @@
+using System;
+using Godot;
+
+public partial class Player : NewScript
+{
+    public override void SetHP(int newHp)
+    {
+        if (playerHP > 0)
+        {
+            GD.Print("inherit");
+            this.playerHP -= newHp;
+            HP.Text = playerHP.ToString();
+        } else
+        {
+            HP.Text = playerHP.ToString(); 
+        }
+    }
+
+    public int GetDamage()
+    {
+        return playerHP;
+    }
+}
