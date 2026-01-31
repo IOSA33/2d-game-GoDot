3e12ace03a3b1f50224e54e2660e6b90898f8eea
IOSA33 <sedinkin33@gmail.com>
2026-01-31 12:48:52 +0200

    latest

--- enemy.gd
+++ enemy.gd
@@ -2,7 +2,10 @@ extends CharacterBody2D
 var speed = 200.0
 @onready var target= $"../CharacterBody2D"
 func _physics_process(_delta: float) -> void:
-	var direction = (target.position - position).normalized()
-	velocity = direction * speed
-	look_at(target.position)
-	move_and_slide() 
+	if is_instance_valid(target):
+		var direction = (target.position - position).normalized()
+		velocity = direction * speed
+		look_at(target.position)
+		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
